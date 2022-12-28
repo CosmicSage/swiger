9f96191eabe687f21b15860b0580b8333b469118
CosmicSage <prabhakarmishra9@yahoo.com>
2022-12-28 22:22:42 +0530

    I lacked inti and patince and wss eluded for hoursd by wat was ringt infront of the eye

---
+++ webMessageXSS.js
@@ -0,0 +1,13 @@
+Hello world!
+
+<iframe src="https://0a2b00bb03c16dddc09c08ff0091001e.web-security-academy.net/" id='ii'></iframe>
+
+
+<iframe src="https://0a2b00bb03c16dddc09c08ff0091001e.web-security-academy.net/" onload="this.contentWindow.postMessage('titts', '*')"></iframe>
+
+
+
+<script>
+var II = document.getElementById('ii').contentWindow;
+II.postMessage('tits', "*");
+</script>
